af2267226ef6d7fb5b9a84698224f684e002dee7
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2025-09-06 14:25:14 +0300

    Add LLM initialization in application lifespan management

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from routes.assets import assets_router
 from routes.projects import projects_router
 from routes.documents import document_router
 from config import settings
+from llm import LLMProviderFactory
 
 
 @asynccontextmanager
@@ -21,6 +22,22 @@ async def lifespan(fastapi_app: FastAPI):
     """
     fastapi_app.state.mongo_client = AsyncMongoClient(str(settings.mongo_uri))
     fastapi_app.state.mongo_db = fastapi_app.state.mongo_client[settings.mongo_db_name]
+
+    llm_factory = LLMProviderFactory(settings)
+    fastapi_app.state.embedding_llm = llm_factory.create(
+        provider_type=settings.embedding_backend
+    )
+    if fastapi_app.state.embedding_llm is not None:
+        fastapi_app.state.embedding_llm.set_embedding_model(
+            settings.embedding_model_id, settings.embedding_dimensions
+        )
+    fastapi_app.state.generation_llm = llm_factory.create(
+        provider_type=settings.generation_backend
+    )
+    if fastapi_app.state.generation_llm is not None:
+        fastapi_app.state.generation_llm.set_generation_model(
+            settings.generation_model_id
+        )
 
     yield
 
