0b210fca3d08144e912a0de5cdf0c9245564f1ed
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2025-09-09 16:26:14 +0300

    Add RetrievedDocumentChunk model definition for document chunk representation

---
+++ src/models/vector.py
@@ -0,0 +1,22 @@
+"""
+Model defnition for a retrieved document chunk.
+"""
+
+from typing import Any, Dict, Optional
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class RetrievedDocumentChunk(BaseModel):
+    """Model definition for retrieved document chunk."""
+
+    id: str | int = Field(..., description="The unique identifier for the vector")
+    text: str = Field(..., description="The text content of the document chunk")
+    metadata: Dict[str, Any] = Field(
+        ..., description="Metadata associated with the document chunk"
+    )
+    score: Optional[float] = Field(
+        default=None,
+        description="The relevance score of the document chunk in case of similarity search",
+    )
+
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="ignore")
