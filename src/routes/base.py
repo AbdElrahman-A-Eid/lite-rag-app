b03c842ed47515110a31ac196b9839e53e11a7a7
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2025-09-09 16:23:06 +0300

    Refactor root endpoint to accept Request parameter for accessing application settings

--- src/routes/base.py
+++ src/routes/base.py
@@ -3,19 +3,19 @@ Base API routes for Lite-RAG-App
 """
 
 from typing import Dict
-from fastapi import APIRouter
-from config import settings
+from fastapi import APIRouter, Request
 
 base_router = APIRouter(prefix="/api/v1", tags=["base", "v1"])
 
 
 @base_router.get("/")
-async def root() -> Dict[str, str]:
+async def root(request: Request) -> Dict[str, str]:
     """Root endpoint
 
     Returns:
         dict: Root status
     """
+    settings = request.app.state.settings
     return {"App Name": settings.app_name, "App Version": settings.app_version}
 
 
