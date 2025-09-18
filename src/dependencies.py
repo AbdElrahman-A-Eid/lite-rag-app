f375304899053cebb05b26d095ae6311e13c0f98
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2025-09-18 15:56:15 +0300

    Add dependency function to inject SQLAlchmy db session

--- src/dependencies.py
+++ src/dependencies.py
@@ -6,6 +6,24 @@ from typing import AsyncGenerator
 
 from fastapi import Request
 from pymongo import AsyncMongoClient
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+)
+
+
+async def get_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
+    """Get a database session from the request state.
+
+    Args:
+        request (Request): The FastAPI request object.
+
+    Returns:
+        AsyncGenerator[AsyncSession, None]: The database session.
+    """
+    Session: async_sessionmaker = request.app.state.async_session
+    async with Session() as session:
+        yield session
 
 
 async def get_db(request: Request) -> AsyncGenerator[AsyncMongoClient, None]:
