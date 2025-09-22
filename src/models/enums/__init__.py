5629b9e99bf3fac8160a1c499b62a7d0fcbfe6d0
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2025-09-22 10:26:41 +0300

    Remove MongoDB-specific CollectionNames enumeration

--- src/models/enums/__init__.py
+++ src/models/enums/__init__.py
@@ -3,7 +3,6 @@ Enumerations for the application.
 """
 
 from models.enums.assets import AssetType
-from models.enums.collections import CollectionNames
 from models.enums.documents import DocumentFileType
 from models.enums.log_level import LogLevel
 from models.enums.responses import ResponseSignals
