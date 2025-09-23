84369501bc7ac4b4b7d44cad470e8bfefca264ac
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2025-09-23 13:32:58 +0300

    Refactor database session management to remove MongoDB client and enhance error handling

--- src/dependencies.py
+++ src/dependencies.py
@@ -2,10 +2,10 @@
 Dependencies for the application.
 """
 
+import logging
 from typing import AsyncGenerator
 
 from fastapi import Request
-from pymongo import AsyncMongoClient
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -21,18 +21,13 @@ async def get_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
     Returns:
         AsyncGenerator[AsyncSession, None]: The database session.
     """
+    logger = logging.getLogger("dependencies")
     Session: async_sessionmaker = request.app.state.async_session
     async with Session() as session:
-        yield session
-
-
-async def get_db(request: Request) -> AsyncGenerator[AsyncMongoClient, None]:
-    """Get the database client from the request state.
-
-    Args:
-        request (Request): The FastAPI request object.
-
-    Returns:
-        AsyncGenerator[AsyncMongoClient]: The MongoDB client.
-    """
-    yield request.app.state.mongo_db
+        try:
+            yield session
+            await session.commit()
+            logger.debug("Transaction committed successfully")
+        except Exception as e:
+            logger.error("Error occurred, rolling back: %s", str(e))
+            await session.rollback()
